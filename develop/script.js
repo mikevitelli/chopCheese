4d77b855022fb0466b86b62fb534a9dfbff1a8f1
Mike <mike@mikevitelli.com>
2020-10-15 12:04:46 -0400

    youtube needs better quotas

--- develop/script.js
+++ develop/script.js
@@ -72,5 +72,36 @@ $.ajax({
 }).then(function (response) {
   // Console log info from 5 videos
   console.log(response);
+  //log first videos ID
   console.log(response.items[0].id.videoId);
+  // $("#youtube").attr("src", "https://www.youtube.com/embed/Xi28pEbMdTw");
 });
+
+var videoID = "Xi28pEbMdTw";
+
+if (window.innerWidth > 500) {
+  // Video background script
+  // Loads the YouTube IFrame API JavaScript code.
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  // Inserts YouTube JS code into the page.
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+  // onYouTubeIframeAPIReady() is called when the IFrame API is ready to go.
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+      videoId: videoID, // the ID of the video (mentioned above)
+      playerVars: {
+        autoplay: 1, // start automatically
+        controls: 0, // don't show the controls (we can't click them anyways)
+        modestbranding: 1, // show smaller logo
+        loop: 1, // loop when complete
+        mute: 1,
+        cc_load_policy: 0, // Hide closed captions
+        iv_load_policy: 3,
+        playlist: "67w--Fn04xg", // required for looping, matches the video ID
+      },
+    });
+  }
+}
